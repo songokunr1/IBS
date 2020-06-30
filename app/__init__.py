dbbf2dfe0f6fe84bd39aaf2dbd421eae482eb5f5
songokunr1 <pawelm18@gmail.com>
2020-06-30 20:38:07 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CsrfProtect
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from flask_cors import CORS
 
 from werkzeug.wsgi import DispatcherMiddleware
 from werkzeug.serving import run_simple
@@ -10,13 +12,14 @@ from werkzeug.serving import run_simple
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:hattrick@localhost/ibs'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')'postgresql://postgres:hattrick@localhost/ibs'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['JWT_TOKEN_LOCATION'] = ['cookies']
 app.config['WTF_CSRF_ENABLED'] = False
 
+CORS(app, resources={r'/*': {'origins': '*'}})
 db = SQLAlchemy(app)
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 session = Session(engine)
