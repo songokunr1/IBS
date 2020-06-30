b3a9dbabb0bba0f255aa17deae76ac2c9b07528e
songokunr1 <pawelm18@gmail.com>
2020-06-30 23:03:56 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ from werkzeug.serving import run_simple
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')'postgresql://postgres:hattrick@localhost/ibs'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://postgres:hattrick@localhost/ibs')
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
