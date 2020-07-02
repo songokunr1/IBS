314dccca9a6e5f7b20fe75ec28cf861ec0fcadea
songokunr1 <pawelm18@gmail.com>
2020-07-02 18:05:31 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from app import app
 from app import db
-
+import os
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
     # app.run(port=8020, debug=True)
 
 
