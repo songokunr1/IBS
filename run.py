87093b432356044117f7bf6fa1f5e2e260ea3993
songokunr1 <pawelm18@gmail.com>
2020-07-09 21:25:32 +0200

    Update run.py

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ from app import db
 import os
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-#    app.run(port=8020, debug=True)
+    # app.run(host='0.0.0.0', port=port)
+    app.run(port=8020, debug=True)
 
 
 @app.before_first_request
