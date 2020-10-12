71fae1b2389d14e52409cbe4c9d8e5081c026fc1
songokunr1 <pawelm18@gmail.com>
2020-10-12 23:49:08 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CsrfProtect
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from app.blueprint import blueprint
+
 # from flask_cors import CORS
 #
 # from werkzeug.wsgi import DispatcherMiddleware
@@ -11,6 +13,7 @@ from sqlalchemy.orm import Session
 
 
 app = Flask(__name__)
+app.register_blueprint(blueprint, url_prefix="/js")
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://postgres:hattrick@localhost/ibs')
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
