9ec4dde16701238118f1299e7845f55e3b6945fc
songokunr1 <pawelm18@gmail.com>
2020-10-14 09:19:47 +0200

    Create helpers.py

---
+++ app/helpers.py
@@ -0,0 +1,14 @@
+from datetime import date, timedelta
+import datetime as dt
+
+
+def get_date_info(date_string):
+    datetime = dt.datetime.strptime(date_string, "%Y-%m-%d")
+    date_info = {
+        'previous': str(datetime + timedelta(days=-1))[:10],
+        'next': str(datetime + timedelta(days=1))[:10],
+        'today': datetime,
+        'week_name': datetime.strftime("%A"),
+        'info': str(datetime.strftime("%A")) + ' ' + str(datetime)[:10]
+    }
+    return date_info
