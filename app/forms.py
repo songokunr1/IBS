4e0ad672ee44814bdaf60909fc051499765a9f99
songokunr1 <pawelm18@gmail.com>
2020-10-21 22:32:24 +0200

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, BooleanField, TextAreaField, HiddenField, SelectMultipleField, SelectField, StringField
+from wtforms import SubmitField, BooleanField, TextAreaField, HiddenField, SelectMultipleField, SelectField, StringField,PasswordField
 from wtforms.fields.html5 import DateField
-from wtforms.validators import DataRequired
-
-from app.models import DateNew,Category, Activity, Meal
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import DateNew,Category, Activity, Meal, User
 
 
 def choices_by_category(*category):
@@ -122,6 +121,33 @@ class DeleteMeal(FlaskForm):
         self.meal_id.choices = [(single['id'], single['name']) for single in self.all_meals]
     meal_id = SelectField('Type', coerce=int)
     submit_delete_meal = SubmitField()
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
 
 # class ChooseMultiple(FlaskForm):
 #     filter = FilterField
