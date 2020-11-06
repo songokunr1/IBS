0a7a82a872736db96d6034bc40dc73b77ee9c6c0
songokunr1 <pawelm18@gmail.com>
2020-11-06 16:57:25 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 # from app.blueprint import blueprint
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-# from flask_migrate import Migrate
+from flask_migrate import Migrate
 
 # from flask_cors import CORS
 #
@@ -20,7 +20,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://postgres:hattrick@localhost/ibs')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://postgres:hattrick@localhost/test_ibs')
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://postgres:hattrick@localhost/new_empty')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['JWT_TOKEN_LOCATION'] = ['cookies']
@@ -34,7 +33,6 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-# migrate = Migrate(app, db)
 
 csrf = CsrfProtect()
 from app import routes
