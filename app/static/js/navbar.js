4c1a8d2c11ff52abaa122cada4a3b1a382eb5667
songokunr1 <pawelm18@gmail.com>
2020-11-09 20:37:10 +0100

    Update navbar.js

--- app/static/js/navbar.js
+++ app/static/js/navbar.js
@@ -1,5 +1,15 @@
-$("#menu-toggle").click(function(e) {
-  e.preventDefault();
-  $("#wrapper").toggleClass("toggled");
-});
-//alert("Hello! I am an alert box!!");
+//$("#menu-toggle").click(function(e) {
+//  e.preventDefault();
+//  $("#wrapper").toggleClass("toggled");
+//alert("Hello! I am an alert box!!");   background-color: #2574A9;
+const toggle = document.getElementById("menu-toggle")
+const myNode = document.getElementById("wrapper");
+const el = myNode.innerHTML
+console.log(toggle)
+toggle.onclick = () => {
+  if (myNode.hasChildNodes()) {
+  myNode.innerHTML = '';}
+  else {
+  myNode.innerHTML = el;
+  }
+}
