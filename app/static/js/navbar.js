a4c53f79a5866bf932da46ed0ac0da3d588fc0d5
songokunr1 <pawelm18@gmail.com>
2020-11-24 20:51:16 +0100

    Update navbar.js

--- app/static/js/navbar.js
+++ app/static/js/navbar.js
@@ -1,11 +1,14 @@
 var toggle = document.getElementById("menu-toggle")
 var myNode = document.getElementById("wrapper");
+var menuu = document.getElementById("place_for_menu");
 var el = myNode.innerHTML
 console.log(toggle)
 toggle.onclick = () => {
   if (myNode.hasChildNodes()) {
-  myNode.innerHTML = '';}
+  myNode.innerHTML = '';
+  var toggle = document.getElementById("menu-toggle");}
   else {
+  menuu.innerHTML = '';
   myNode.innerHTML = el;
   }
 }
