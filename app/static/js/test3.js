751ef1add67c34f00530f1a2374aa46b663607bb
songokunr1 <pawelm18@gmail.com>
2020-11-24 20:52:15 +0100

    Create test3.js

---
+++ app/static/js/test3.js
@@ -0,0 +1,10 @@
+console.log('hello! js welcome')
+window.onload=function(){
+  const btnbutton = document.querySelector('.clean-up')
+  console.log(btnbutton)
+
+  btnbutton.addEventListener('click', e => {
+
+    console.log("btnbutton")
+    });
+}
